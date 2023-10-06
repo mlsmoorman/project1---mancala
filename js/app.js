a7151dbe61b8179911e069a7c757b927fc6fb664
mlsmoorman <melissa.s.moorman@gmail.com>
2023-10-06 13:29:16 -0500

    add event listener for selection

--- js/app.js
+++ js/app.js
@@ -8,51 +8,77 @@ const POCKETS = [
     "A4",
     "A5",
     "A6",
-    "AMancala",
     "B1",
     "B2",
     "B3",
     "B4",
     "B5",
     "B6",
-    "BMancala"
+]
+const MANCALA = [
+    "Mancala-A",
+    "Mancala-B"
 ]
 
 /*---------------------------*/
 /*----- STATE VARIABLES -----*/
 /*---------------------------*/
-let scores;
-let winner;
+// let scores;
+// let stones;
 
 /*----------------------------*/                    
 /*----- CACHED ELEMENTS  -----*/
 /*----------------------------*/
-
 //=====  Stones - cache the stones so that they can move from pocket to pocket on the screen
 //=====  Score - cach the player score to update each time a stone lands in a player's Mancala
 //=====  Winner - cache the winner to change the display of who the winner is once game is over
+const pocketsBtn = document.querySelector('#pockets'); // cache selection
+console.log(pocketsBtn)
+
 
 
 /*---------------------------*/
 /*----- EVENT LISTENERS -----*/
 /*---------------------------*/
-
 //==== This section will need an event listener for the click that is within each of the pockets which
 //==== will tell the stones to move from the current pocket to the next pockets
+pocketsBtn.addEventListener('click', handleMove) // logs which button is clicked
 
 /*--------------------------------------------*/
 /*----- CONTROLLER FUNCTIONS DEFINITIONS -----*/
 /*--------------------------------------------*/
-
+init ();
 //==== function init - set the start page of the game and reset it once the replay button is hit
 //==== --   score - initial scores will equal 0
+function init() {
+    scores = { // scores object has 1 property for each player
+        PlayerA: 0,
+        PlayerB: 0
+    }
+    for (i=0; i < MANCALA.length; i++){
+        MANCALA[i] = 0; // sets initial # of stones per mancala to 4
+        console.log(MANCALA[i]) // check working
+    }
+    for (i=0; i < POCKETS.length; i++){
+        POCKETS[i] = 4; // sets initial # of stones per pocket to 4
+        console.log(POCKETS[i]) // check working
+    }
+}
+
+
 //==== --   each pocket A1-A6 and B1-B6 will hold 4 stones
-//==== --   each mancala will start at 0
+//==== --   each mancala will start at 0 stones
 
 //==== function render - change the look of the screen moving stones from one pocket to the next pockets 
 //==== updates the player's score based on how many stones are in the player's mancala and 
 //==== update the screen to show who's turn it is
 
+function handleMove(e) {
+    console.log(e.target.tagName);
+    const chosenPocket = e.target.innerText;
+    console.log(chosenPocket)
+
+}
 //==== function handleChoice - will change the count of stones in the subsequent pockets once choice is made
 //==== will call playerSelect to tell the computer to skip over the oposing player's mancala if applicable
 //==== and will call function update scores and render to update the player screen
